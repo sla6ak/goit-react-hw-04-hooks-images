f1331667220f56315ffec8430e16350b84c473f9
slabak <slabakxaker@gmail.com>
2022-04-18 05:40:25 +0300

    GlobalStyle

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './Components/App';
-import './main.css';
+import '../node_modules/modern-normalize/modern-normalize.css';
+import { GlobalStyle } from 'GlobalStyle.styled';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'; //скелетон загрузок
 import 'react-toastify/dist/ReactToastify.css'; // попапы
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <GlobalStyle/>
   </React.StrictMode>,
   document.getElementById('root')
 );
